3579018ea1fb47a447be165cc7be1f171e7f76f6
Shubham Anand <shubham_chess@live.com>
2019-04-15 09:23:13 +0530

    make keys control use playSound function

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const sound = [new Audio("boom.wav"),new Audio("clap.wav"),new Audio("hihat.wav"),new Audio("kick.wav"),new Audio("openhat.wav"),new Audio("ride.wav"),new Audio("snare.wav"),new Audio("tink.wav"),new Audio("tom.wav")]
 
-playSound=function(i)
-{
+playSound = function(i) {
     document.getElementById(i);    
-    sound[i].currentTime=0;
+    sound[i].currentTime = 0;
     sound[i].play();
     setInterval(frame, 0.1);
     function frame() {
@@ -13,17 +12,17 @@ playSound=function(i)
     
         } else {
              document.getElementById(i).style.cssText='background-color:transparent;color:black;border:solid black';
-            }
-                }
+        }
+    }
 }
 document.addEventListener("keydown",function(e){
-    if(e.keyCode===65){sound[0].currentTime=0;sound[0].play()}
-    else if(e.keyCode===83){sound[1].currentTime=0;sound[1].play()}
-    else if(e.keyCode===68){sound[2].currentTime=0;sound[2].play()}
-    else if(e.keyCode===70){sound[3].currentTime=0;sound[3].play()}
-    else if(e.keyCode===71){sound[4].currentTime=0;sound[4].play()}
-    else if(e.keyCode===72){sound[5].currentTime=0;sound[5].play()}
-    else if(e.keyCode===74){sound[6].currentTime=0;sound[6].play()}
-    else if(e.keyCode===75){sound[7].currentTime=0;sound[7].play()}
-    else if(e.keyCode===76){sound[8].currentTime=0;sound[8].play()}
+    if (e.keyCode===65){playSound(0);}
+    else if(e.keyCode===83){playSound(1);}
+    else if(e.keyCode===68){playSound(2);}
+    else if(e.keyCode===70){playSound(3);}
+    else if(e.keyCode===71){playSound(4);}
+    else if(e.keyCode===72){playSound(5);}
+    else if(e.keyCode===74){playSound(6);}
+    else if(e.keyCode===75){playSound(7);}
+    else if(e.keyCode===76){playSound(8);}
 });
