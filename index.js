310e1b961c3e4e81637fa37d41adedae5ac2fb93
Shubham Anand <shubham_chess@live.com>
2019-04-15 09:38:59 +0530

    fix highlight issue

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 const sound = [new Audio("boom.wav"),new Audio("clap.wav"),new Audio("hihat.wav"),new Audio("kick.wav"),new Audio("openhat.wav"),new Audio("ride.wav"),new Audio("snare.wav"),new Audio("tink.wav"),new Audio("tom.wav")]
 
 playSound = function(i) {
-    document.getElementById(i);    
+    let element = document.getElementById(i);    
+    element.style.cssText='background-color:black;color:white;border:solid white';
     sound[i].currentTime = 0;
     sound[i].play();
-    setInterval(frame, 0.1);
+    let highlightTime = 100;
+    let interval = setInterval(frame, highlightTime);
     function frame() {
-        if (sound[i].seeking) {
-             document.getElementById(i).style.cssText='background-color:black;color:white;border:solid white';
-             clearInterval(id);
-    
-        } else {
-             document.getElementById(i).style.cssText='background-color:transparent;color:black;border:solid black';
+        if (!sound[i].seeking) {
+            element.style.cssText='background-color:transparent;color:black;border:solid black';
+            clearInterval(interval);
         }
     }
 }
+
 document.addEventListener("keydown",function(e){
     if (e.keyCode===65){playSound(0);}
     else if(e.keyCode===83){playSound(1);}
