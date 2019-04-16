3e31375b7536d0dc3232421f4fed1cd6fdfb7bef
nemankyadav <48820915+nemankyadav@users.noreply.github.com>
2019-04-17 00:39:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// drumkit framework
 const sound = [new Audio("boom.wav"),new Audio("clap.wav"),new Audio("hihat.wav"),new Audio("kick.wav"),new Audio("openhat.wav"),new Audio("ride.wav"),new Audio("snare.wav"),new Audio("tink.wav"),new Audio("tom.wav")]
 
 playSound = function(i) {
@@ -15,6 +16,7 @@ playSound = function(i) {
     }
 }
 
+// keybindings to drumkit
 document.addEventListener("keydown",function(e){
     if (e.keyCode===65){playSound(0);}
     else if(e.keyCode===83){playSound(1);}
@@ -26,3 +28,43 @@ document.addEventListener("keydown",function(e){
     else if(e.keyCode===75){playSound(7);}
     else if(e.keyCode===76){playSound(8);}
 });
+
+//recorder
+
+var timeArray=[];
+var j=0;var l=0; k=-1;
+var recordingArray=[];
+var startTime,endTime;
+
+function start() {
+  startTime = new Date();
+};
+function end() {
+    endTime = new Date();
+   return endTime - startTime;
+}
+abc=function(e){
+    if     (e.keyCode===65){recordingArray[j++]='0';console.log(recordingArray.length);timeArray[l++]=end();start()}
+    else if(e.keyCode===83){recordingArray[j++]='1';console.log(recordingArray.length);timeArray[l++]=end();start()}
+    else if(e.keyCode===68){recordingArray[j++]='2';console.log(recordingArray.length);timeArray[l++]=end();start()}
+    else if(e.keyCode===70){recordingArray[j++]='3';console.log(recordingArray.length);timeArray[l++]=end();start()}
+    else if(e.keyCode===71){recordingArray[j++]='4';console.log(recordingArray.length);timeArray[l++]=end();start()}
+    else if(e.keyCode===72){recordingArray[j++]='5';console.log(recordingArray.length);timeArray[l++]=end();start()}
+    else if(e.keyCode===74){recordingArray[j++]='6';console.log(recordingArray.length);timeArray[l++]=end();start()}
+    else if(e.keyCode===75){recordingArray[j++]='7';console.log(recordingArray.length);timeArray[l++]=end();start()}
+    else if(e.keyCode===76){recordingArray[j++]='8';console.log(recordingArray.length);timeArray[l++]=end();start()}
+}
+startRecording=function(){
+    document.addEventListener("keydown",abc);
+    }
+stopRecording=function(){
+    document.removeEventListener("keydown",abc);
+}
+
+playRecording=function(){
+    setTimeout(function(){
+        sound[recordingArray[k]].currentTime=0;
+        sound[recordingArray[k]].play()
+        playRecording()
+    }, timeArray[++k])  
+}
